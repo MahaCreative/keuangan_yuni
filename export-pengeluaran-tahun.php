c6adade106b67bf3f3a6d27178d5c68739005663
MahaCreative <gunturmadjid.3@gmail.com>
2023-09-05 09:36:27 -0700

    update

--- export-pengeluaran-tahun.php
+++ export-pengeluaran-tahun.php
@@ -54,5 +54,10 @@ $html .= '</table>';
 $pdf->writeHTML($html, true, false, true, false, '');
 
 // Close and output PDF
-$pdf->Output('/home/u676389388/domains/pcippnu.com/Data_Pengeluaran_Tahun_' . $tahun . '.pdf', 'F');
+
+$filePath = '/home/u676389388/domains/pcippnu.com/Data_Pengeluaran_Tahun_' . $tahun . '.pdf';
+$pdf->Output($filePath, 'F');
+
+// Redirect to the generated PDF file
+echo '<script>window.location.href = "' . $filePath . '";</script>';
 ?>
