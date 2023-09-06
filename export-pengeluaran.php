173d87ecc61d0fda76f40ca9d4d42f21342ecdcf
MahaCreative <gunturmadjid.3@gmail.com>
2023-09-05 21:11:57 -0700

    update

--- export-pengeluaran.php
+++ export-pengeluaran.php
@@ -55,4 +55,5 @@ $pdf->writeHTML($html, true, false, true, false, '');
 
 // Close and output PDF
 $pdf->Output('/home/u676389388/domains/pcippnu.com/keuangan_yuni/Data_Pengeluaran.pdf', 'F');
+echo '<script>window.location.href = "Data_Pengeluaran.pdf";</script>';
 ?>
